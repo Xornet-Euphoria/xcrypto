0a346ec7d0767d96cd76157079b3e95dae1c0278
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2020-05-07 23:35:37 +0900

    16進表記をエンコード時のエラーをハンドルしてdumpする関数を作成

--- src/xcrypto/util.py
+++ src/xcrypto/util.py
@@ -12,6 +12,23 @@ def num_to_str(num):
     return long_to_bytes(num).decode()
 
 
+# if UnicodeDecodeError is raised, error message (default is "decode error") is dumped
+def dump_hex_str(hex_str, msg="decode error"):
+    try:
+        res = hexstr_to_str(hex_str)
+        print(res)
+    except:
+        print(msg)
+
+
+def dump_num(num, msg="decode error"):
+    try:
+        res = num_to_str(num)
+        print(res)
+    except:
+        print(msg)
+
+
 def prod(num_list):
     return reduce(lambda x, y: x * y, num_list)
 
