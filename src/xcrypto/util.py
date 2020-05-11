ae66c2dc58a61b18d9f1913111927d46b3151710
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2020-05-11 19:27:38 +0900

    フェルマー法と平方に関する関数を追加

--- src/xcrypto/util.py
+++ src/xcrypto/util.py
@@ -1,6 +1,6 @@
 from binascii import unhexlify
 from functools import reduce
-from math import gcd
+from math import gcd, ceil
 from Crypto.Util.number import long_to_bytes
 
 
@@ -35,3 +35,52 @@ def prod(num_list):
 
 def list_gcd(num_list):
     return reduce(gcd, num_list)
+
+
+# return integer number less than or equal to pow(x, (1/n))
+def int_nth_root(x, n):
+    b_length = x.bit_length()
+    ret_ceil = pow(2, ceil(b_length / n))
+    ret_range = [1, ret_ceil]
+    while True:
+        ret_half = (ret_range[0] + ret_range[1]) // 2
+        v = pow(ret_half, n)
+        if v < x:
+            if pow(ret_half + 1, n) > x:
+                return ret_half
+            ret_range[0] = ret_half
+        elif v > x:
+            ret_range[1] = ret_half
+        elif v == x:
+            return ret_half
+
+
+def int_sqrt(x):
+    return int_nth_root(x, 2)
+
+
+def is_square(x):
+    if x < 0:
+        return False
+
+    r = int_sqrt(x)
+
+    return r**2 == x
+
+
+def fermat_method(n):
+    a = int_sqrt(n)
+
+    if a**2 == n:
+        return (a, a)
+
+    a += 1
+    b_pow_2 = a ** 2 - n
+
+    while not is_square(b_pow_2):
+        a += 1
+        b_pow_2 = a ** 2 - n
+
+    b = int_sqrt(b_pow_2)
+
+    return (a + b, a - b)
